9ee8703d5586b08bde6c0dd4de1862e66f0c41ec
Michael Ikoko <michaelikoko.o@gmail.com>
2023-01-21 22:40:28 +0100

    Changed settings/dev.py, settings/production.py

--- food_blog/settings/dev.py
+++ food_blog/settings/dev.py
@@ -10,16 +10,8 @@ SECRET_KEY = "django-insecure-i!hit*$g9ypx_^hch#=^m%+9b1wqfqkja=vz3&zp0+zb-=3+3g
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ["*"]
 
-#EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'the9jafoodblog@gmail.com'
-EMAIL_HOST_PASSWORD = 'sftvkflzfhqobuqg'
-#EMAIL_PORT = 587
-#EMAIL_USE_TLS = True
-EMAIL_PORT = 465
-EMAIL_USE_SSL = True
 
 DATABASES = {
     "default": {
--- food_blog/settings/production.py
+++ food_blog/settings/production.py
@@ -26,8 +26,8 @@ DATABASES = {
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'the9jafoodblog@gmail.com'
-EMAIL_HOST_PASSWORD = 'sftvkflzfhqobuqg'
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
 #EMAIL_PORT = 587
 #EMAIL_USE_TLS = True
 EMAIL_PORT = 465
